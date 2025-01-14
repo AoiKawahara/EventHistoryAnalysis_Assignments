a41edee2be5c2a57cc25116eda054dc101cbf08a
AoiKawahara <aoikawakawa@gmail.com>
2025-01-14 19:10:43 +0100

    3.R - Discrete-time competing risk

--- Assignment3.R
+++ Assignment3.R
@@ -129,7 +129,7 @@ legend(x = "bottomright",
        col=c(2,3,4,5),
        lty=c(1,1))
 
-coxActCan<- coxph(Surv(data$MONTH_CONT, data$CANCER) ~ as.factor(ACTIVITY), data = data)
+coxActCan<- coxph(Surv(MONTH_CONT, CANCER) ~ 1 + as.factor(ACTIVITY), data = data.subacts)
 summary(coxActCan)
 
 plot(survfit(coxActCan, newdata = data.frame(ACTIVITY = 1)),
@@ -164,10 +164,10 @@ legend(x = "bottomright",
        legend=c("1.Employed", "2.Unemployed", "3.Retired", "4.Disabled"),
        col=c("red", "yellow", "lightgreen", "blue"), lty=c(1,1))
 
-coxCan <- coxph(Surv(MONTH_CONT, CANCER) ~ as.factor(ACTIVITY), data = data.subacts)
+coxCan <- coxph(Surv(MONTH_CONT, CANCER) ~ 1, data = data.subacts)
 summary(coxCan)
 
-test_coxCancoxActCan <- anova(coxCan, coxActCan, test = "LRT")
+test_coxCancoxActCan <- anova(coxActCan, coxCan, test = "LRT")
 print(test_coxCancoxActCan)
 
 #### CIRCULAR ####
@@ -191,7 +191,7 @@ legend(x = "bottomright",
        col=c(2,3,4,5),
        lty=c(1,1))
 
-coxActCir <- coxph(Surv(data$MONTH_CONT, data$CIRCULAR) ~ as.factor(ACTIVITY), data = data)
+coxActCir <- coxph(Surv(MONTH_CONT, CIRCULAR) ~ 1 + as.factor(ACTIVITY), data = data.subacts)
 summary(coxActCir)
 
 plot(survfit(coxActCir, newdata = data.frame(ACTIVITY = 1)),
@@ -226,7 +226,7 @@ legend(x = "bottomright",
        legend=c("1.Employed", "2.Unemployed", "3.Retired", "4.Disabled"),
        col=c("red", "yellow", "lightgreen", "blue"), lty=c(1,1))
 
-coxCir <- coxph(Surv(MONTH_CONT, CIRCULAR) ~ as.factor(ACTIVITY), data = data.subacts)
+coxCir <- coxph(Surv(MONTH_CONT, CIRCULAR) ~ 1, data = data.subacts)
 summary(coxCir)
 
 test_coxCircoxActCir <- anova(coxCir, coxActCir, test = "LRT")
@@ -253,7 +253,7 @@ legend(x = "bottomright",
        col=c(2,3,4,5),
        lty=c(1,1))
 
-coxActRes <- coxph(Surv(data$MONTH_CONT, data$RESPIRATORY) ~ as.factor(ACTIVITY), data = data)
+coxActRes <- coxph(Surv(MONTH_CONT, RESPIRATORY) ~ 1 + as.factor(ACTIVITY), data = data.subacts)
 summary(coxActRes)
 
 plot(survfit(coxActRes, newdata = data.frame(ACTIVITY = 1)),
@@ -288,7 +288,7 @@ legend(x = "bottomright",
        legend=c("1.Employed", "2.Unemployed", "3.Retired", "4.Disabled"),
        col=c("red", "yellow", "lightgreen", "blue"), lty=c(1,1))
 
-coxRes <- coxph(Surv(MONTH_CONT, RESPIRATORY) ~ as.factor(ACTIVITY), data = data.subacts)
+coxRes <- coxph(Surv(MONTH_CONT, RESPIRATORY) ~ 1, data = data.subacts)
 summary(coxRes)
 
 test_coxRescoxActRes <- anova(coxRes, coxActRes, test = "LRT")
@@ -316,7 +316,7 @@ legend(x = "bottomright",
        col=c(2,3,4,5),
        lty=c(1,1))
 
-coxActOth <- coxph(Surv(data$MONTH_CONT, data$OTHER) ~ as.factor(ACTIVITY), data = data)
+coxActOth <- coxph(Surv(MONTH_CONT, OTHER) ~ 1 + as.factor(ACTIVITY), data = data.subacts)
 summary(coxActOth)
 
 plot(survfit(coxActOth, newdata = data.frame(ACTIVITY = 1)),
@@ -351,7 +351,7 @@ legend(x = "bottomright",
        legend=c("1.Employed", "2.Unemployed", "3.Retired", "4.Disabled"),
        col=c("red", "yellow", "lightgreen", "blue"), lty=c(1,1))
 
-coxOth <- coxph(Surv(MONTH_CONT, OTHER) ~ as.factor(ACTIVITY), data = data.subacts)
+coxOth <- coxph(Surv(MONTH_CONT, OTHER) ~ 1, data = data.subacts)
 summary(coxOth)
 
 test_coxOthcoxActOth <- anova(coxOth, coxActOth, test = "LRT")
@@ -366,17 +366,47 @@ data.ppf$CAUSE_fac <- factor(data.ppf$CAUSE,
                          labels=c("0.Survival or emigration", "1.Cancer", "2.Circular", "3.Respiratory", "4.Other"))
 table(data.ppf$EXPOSURE, data.ppf$CAUSE_fac)
 
-#### 2b ####
-multinomAct <- multinom(CAUSE ~ 1 + as.factor(EXPOSURE) + as.factor(ACTIVITY), data = data.ppf)
-summary(multinomAct)
-coef(multinomAct)
-exp(coef(multinomAct))
+#### 2b 2c ####
+glmcan = glm(CANCER ~ 1 + EXPOSURE, data=data.ppf, family=binomial(link = "logit"))
+summary(glmcan)
+glmActcan = glm(CANCER ~ 1 + EXPOSURE + as.factor(ACTIVITY), data=data.ppf, family=binomial(link = "logit"))
+summary(glmActcan)
 
-multinomNull <- multinom(CAUSE ~ 1 + as.factor(EXPOSURE), data = data.ppf)
-summary(multinomNull)
+test_glmcanglmActcan <- anova(glmcan,glmActcan, test = "LRT")
+print(test_glmcanglmActcan)
 
-test_multinomActNull <- anova(multinomNull, multinomAct)
-print(test_multinomActNull)
+coxCanPpf <- coxph(Surv(EXPOSURE, CANCER) ~ 1 + as.factor(ACTIVITY), data = data.ppf)
+summary(coxCanPpf)
 
-#### 2c ####
+glmcir = glm(CIRCULAR ~ 1 + EXPOSURE, data=data.ppf, family=binomial(link = "logit"))
+summary(glmcir)
+glmActcir = glm(CIRCULAR ~ 1 + EXPOSURE + as.factor(ACTIVITY), data=data.ppf, family=binomial(link = "logit"))
+summary(glmActcir)
 
+test_glmcirglmActcir <- anova(glmcir,glmActcir, test = "LRT")
+print(test_glmcirglmActcir)
+
+coxCirPpf <- coxph(Surv(EXPOSURE, CIRCULAR) ~ 1 + as.factor(ACTIVITY), data = data.ppf)
+summary(coxCirPpf)
+
+glmres = glm(RESPIRATORY ~ 1 + EXPOSURE, data=data.ppf, family=binomial(link = "logit"))
+summary(glmres)
+glmActres = glm(RESPIRATORY ~ 1 + EXPOSURE + as.factor(ACTIVITY), data=data.ppf, family=binomial(link = "logit"))
+summary(glmActres)
+
+test_glmresglmActres <- anova(glmres,glmActres, test = "LRT")
+print(test_glmresglmActres)
+
+coxResPpf <- coxph(Surv(EXPOSURE, RESPIRATORY) ~ 1 + as.factor(ACTIVITY), data = data.ppf)
+summary(coxResPpf)
+
+glmoth = glm(OTHER ~ 1 + EXPOSURE, data=data.ppf, family=binomial(link = "logit"))
+summary(glmoth)
+glmActoth = glm(OTHER ~ 1 + EXPOSURE + as.factor(ACTIVITY), data=data.ppf, family=binomial(link = "logit"))
+summary(glmActoth)
+
+test_glmothglmActoth <- anova(glmoth,glmActoth, test = "LRT")
+print(test_glmothglmActoth)
+
+coxOthPpf <- coxph(Surv(EXPOSURE, OTHER) ~ 1 + as.factor(ACTIVITY), data = data.ppf)
+summary(coxOthPpf)
